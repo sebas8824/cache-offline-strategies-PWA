61d49158edca4f737c941315ddf33d6b9d41c826
sebas8824 <sebastian.benitez@outlook.com>
2018-11-17 18:44:58 -0500

    Respuesta offline básica

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
+self.addEventListener('fetch', event => {
+    const offlineResponse = new Response(`
+        Bienvenido a mi pagina web
 
+        Disculpa pero para usarla, necesitas internet   
+    `);
+
+    const respuesta = fetch(event.request)
+        .catch(() => offlineResponse);
+
+    event.respondWith(respuesta);
+})
 
 
