202b5c947970f87d917fc4c13a03d6b6199f5376
sebas8824 <sebastian.benitez@outlook.com>
2018-11-17 18:54:51 -0500

    Respuesta HTML string

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
 self.addEventListener('fetch', event => {
+    
     const offlineResponse = new Response(`
-        Bienvenido a mi pagina web
-
-        Disculpa pero para usarla, necesitas internet   
-    `);
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Mi PWA</title>
+        </head>                
+        <body class="container p-3">
+            <h1>Modo offline</h1>
+        </body>
+    </html>
+    `, {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    });
+    
+    // Requiere estrategia del cache para traer el archivo creado.s
+    //const offlineResponse = fetch('pages/offline.html');
 
     const respuesta = fetch(event.request)
         .catch(() => offlineResponse);
